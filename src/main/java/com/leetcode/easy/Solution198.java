aaf309155a94d48ca3acd8cee6c436b716ac13f1
rxf <178459897@qq.com>
2018-09-17 15:52:12 +0800

    172/198

--- src/main/java/com/leetcode/easy/Solution198.java
+++ src/main/java/com/leetcode/easy/Solution198.java
@@ -22,20 +22,27 @@ package com.leetcode.easy;
  */
 public class Solution198 {
     public static void main(String[] args) {
-        int[] nums = {2, 7, 9, 3, 1};
+//        int[] nums = {2, 1, 1, 2};
+        int[] nums = {226, 174, 214, 16, 218, 48, 153, 131, 128, 17, 157, 142, 88, 43, 37, 157, 43, 221, 191, 68, 206, 23, 225, 82, 54, 118, 111, 46, 80, 49, 245, 63, 25, 194, 72, 80, 143, 55, 209, 18, 55, 122, 65, 66, 177, 101, 63, 201, 172, 130, 103, 225, 142, 46, 86, 185, 62, 138, 212, 192, 125, 77, 223, 188, 99, 228, 90, 25, 193, 211, 84, 239, 119, 234, 85, 83, 123, 120, 131, 203, 219, 10, 82, 35, 120, 180, 249, 106, 37, 169, 225, 54, 103, 55, 166, 124};
+//        int[] nums = {2, 7, 9, 3, 1};
         System.out.println(rob(nums));
     }
 
     public static int rob(int[] nums) {
-        int sum1 = 0, sum2 = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (i % 2 == 0) {
-                sum2 += nums[i];
-            } else {
-                sum1 += nums[i];
+        if (nums == null || nums.length == 0) {
+            return 0;
+        } else if (nums.length == 1) {
+            return nums[0];
+        } else if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        } else {
+            int[] cache = new int[nums.length];
+            cache[0] = nums[0];
+            cache[1] = Math.max(nums[0], nums[1]);
+            for (int i = 2; i < nums.length; i++) {
+                cache[i] = Math.max(nums[i] + cache[i - 2], cache[i - 1]);
             }
+            return cache[cache.length - 1];
         }
-
-        return sum1 > sum2 ? sum1 : sum2;
     }
 }
